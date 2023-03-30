83ba3aef23acc12ad9a476cc8ea8ab0cea11107d
edmooliveira29 <edmooliveira29@gmail.com>
2023-03-30 14:37:57 -0300

    config: change indentation

--- src/entities/user/user.ts
+++ src/entities/user/user.ts
@@ -1,10 +1,9 @@
 export class UserEntity {
-    public readonly name: string
-    public readonly password: string
-    constructor(name: string, password: string) {
-        this.name = name
-        this.password = password
-        Object.freeze(true)
-    }
+  public readonly name: string
+  public readonly password: string
+  constructor (name: string, password: string) {
+    this.name = name
+    this.password = password
+    Object.freeze(this)
+  }
 }
-
