6d24f9df3e47e5b22e3b6f2612aa316fe1135382
edmooliveira29 <edmooliveira29@gmail.com>
2023-03-31 14:23:27 -0300

    feat: delete file users.ts and yout test

--- src/entities/user/user.test.ts
+++
@@ -1,7 +0,0 @@
-import { type UserData } from './data/user-data'
-import { UserEntity } from './user'
-test('Should return entity of the user', () => {
-  const users: UserData = { email: 'user@email.com', name: 'user', password: 'password' }
-  const userEntity = new UserEntity(users)
-  expect(userEntity.users).toEqual(users)
-})
--- src/entities/user/user.ts
+++
@@ -1,9 +0,0 @@
-import { type UserData } from './data/user-data'
-
-export class UserEntity {
-  public readonly users: UserData
-  constructor (users: UserData) {
-    this.users = users
-    Object.freeze(this)
-  }
-}
