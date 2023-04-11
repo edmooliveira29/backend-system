71cdb3b5081bc58a19f8e501f30d257a474eca3b
edmooliveira29 <edmooliveira29@gmail.com>
2023-04-11 15:14:40 -0300

    feat: create config to express

--- src/main/config/app.js
+++ src/main/config/app.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import setupMiddleware from './middleware/middleware'
+import setupRoutes from './routes'
 
 const app = express()
-
+setupMiddleware(app)
 setupRoutes(app)
+
+export default app
