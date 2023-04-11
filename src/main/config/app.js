ee613dbd517fd2cf968eca89095825e9e890d23b
edmooliveira29 <edmooliveira29@gmail.com>
2023-04-11 14:57:10 -0300

    feat: create router to express with routes

---
+++ src/main/config/app.js
@@ -0,0 +1,5 @@
+import express from 'express'
+
+const app = express()
+
+setupRoutes(app)
---
+++ src/main/config/routes.ts
@@ -0,0 +1,12 @@
+import { type Express, Router } from 'express'
+import { readdirSync } from 'fs'
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api/v1', router)
+  // eslint-disable-next-line n/no-path-concat
+  readdirSync(`${__dirname}/..routes`).map(async file => {
+    if (!file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
+  })
+}
