89bd5c144fa2d38191cdf84961715bfa21fbe989
edmooliveira29 <edmooliveira29@gmail.com>
2023-05-09 21:33:27 -0300

    fix: enable acess to site

--- src/main/config/middleware/middleware.ts
+++ src/main/config/middleware/middleware.ts
@@ -2,8 +2,10 @@ import { type Express } from 'express'
 import { bodyParser } from './body-parser'
 import { cors } from './cors'
 import { contentType } from './content-type'
+import { corsSite } from './corsSite'
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(corsSite)
   app.use(contentType)
 }
