eb10eadfd8312d370e5c1fe2058f1346d9cd1c71
edmooliveira29 <edmooliveira29@gmail.com>
2023-09-08 18:01:33 -0300

    fix: enable morgan-body

--- src/main/config/middleware/middleware.ts
+++ src/main/config/middleware/middleware.ts
@@ -2,14 +2,14 @@ import { type Express } from 'express'
 import { bodyParser } from './body-parser'
 import { contentType } from './content-type'
 import { corsGeneral, corsOptions, corsOrigin } from './cors'
-// import morganBody from 'morgan-body'
+import morganBody from 'morgan-body'
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(contentType)
   app.use(corsGeneral)
   app.use(corsOrigin)
   app.use(corsOptions)
-  // morganBody(app, {
-  //   theme: 'dimmed'
-  // })
+  morganBody(app, {
+    theme: 'dimmed'
+  })
 }
