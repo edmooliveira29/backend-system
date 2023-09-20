cf5088cca099c552d5723b2d41877f7ec02bf33e
edmooliveira29 <edmooliveira29@gmail.com>
2023-09-20 18:51:04 -0300

    fix: insert condition to insert users

--- src/infra/helpers/mongo-helper.ts
+++ src/infra/helpers/mongo-helper.ts
@@ -4,6 +4,7 @@ export const MongoConnection = {
   client: null as unknown as MongoClient,
 
   async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri)
     const username = process.env.MONGO_URL_USERNAME
     const password = process.env.MONGO_URL_PASSWORD
     let options: any = {}
@@ -15,7 +16,22 @@ export const MongoConnection = {
         }
       }
     }
-    this.client = await MongoClient.connect(uri, options)
+    try {
+      const adminDb = this.client.db('system-database')
+      const users = await adminDb.command({ usersInfo: username })
+      if (users.users.length === 0) {
+        await adminDb.command({
+          createUser: 'system-user-database',
+          pwd: password,
+          roles: [{ role: 'dbOwner', db: 'system-database' }]
+        })
+      }
+      await this.disconnect()
+      this.client = await MongoClient.connect(uri, options)
+    } catch (err) {
+      console.error('Erro ao criar usuário no MongoDB:', err)
+      throw err
+    }
   },
   async disconnect (): Promise<void> {
     if (this.client) {
